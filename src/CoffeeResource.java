c802a419858f96271099ff5ff7c72f56419e4f04
Bojan Pađen <page0itv@gmail.com>
2024-10-31 14:29:42 +0100

    modify class to be instanced

--- src/CoffeeResource.java
+++ src/CoffeeResource.java
@@ -1,8 +1,48 @@
 public class CoffeeResource {
 
-    public static int water = 400;
-    public static int milk = 540;
-    public static int beans = 120;
-    public static int cups = 9;
-    public static int money = 550;
+    private int water = 400;
+    private int milk = 540;
+    private int beans = 120;
+    private int cups = 9;
+    private int money = 550;
+
+    public int getWater() {
+        return water;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public int getBeans() {
+        return beans;
+    }
+
+    public int getCups() {
+        return cups;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setWater(int water) {
+        this.water = water;
+    }
+
+    public void setMilk(int milk) {
+        this.milk = milk;
+    }
+
+    public void setBeans(int beans) {
+        this.beans = beans;
+    }
+
+    public void setCups(int cups) {
+        this.cups = cups;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
