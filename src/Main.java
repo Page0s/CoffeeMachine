2655ddba527e938bc036f090b49bdb699f685d08
Bojan Pađen <page0itv@gmail.com>
2024-10-31 14:28:49 +0100

    modify method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        CoffeeMachineLogic coffeeMachineLogic = new CoffeeMachineLogic();
+
+        CoffeeMachineUI ui = new CoffeeMachineUI();
+        ui.runCoffeeMachine();
     }
 }
